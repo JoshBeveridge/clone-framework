942e01d0a08a56ac91cd216368a54c904359b964
Josh Beveridge <Josh Beveridge>
2017-04-09 17:11:28 -0300

    New cross OS notifications

--- assets/notify.js
+++ assets/notify.js
@@ -1,12 +1,9 @@
-// https://shapeshed.com/writing-cross-platform-node/#scripts-in-package-json
-// https://dzone.com/articles/execute-unix-command-nodejs
+const notifier = require('node-notifier');
+// String
+notifier.notify('Message');
 
-var os = require('os');
-// var sys = require('sys')
-var exec = require('child_process').exec;
-// function puts(error, stdout, stderr) { sys.puts(stdout) }
-// exec("ls -la", puts);
-
-if(os.platform() == 'darwin') {
-    exec("terminal-notifier -message 'All done! 🐱'");
-};
+// Object
+notifier.notify({
+  'title': 'My notification',
+  'message': 'Hello, there!'
+});
