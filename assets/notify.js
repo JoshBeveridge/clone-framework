6466734505cefdb1e29ae24f922091e6e520f578
Josh Beveridge <imjoshdrink@gmail.com>
2017-04-09 17:22:42 -0300

    Updating notification text

--- assets/notify.js
+++ assets/notify.js
@@ -1,9 +1,9 @@
 const notifier = require('node-notifier');
 // String
-notifier.notify('Message');
+// notifier.notify('Message');
 
 // Object
 notifier.notify({
-  'title': 'My notification',
-  'message': 'Hello, there!'
+  'title': "It's 5 o'clock somewhere!",
+  'message': 'Clone finished compiling your CSS. 🐈'
 });
